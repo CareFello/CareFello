4b7d5bf993e95fc95f6f9a4c6dec37f3753703b1
praneeth <praneeth495@gmail.com>
2023-07-13 11:06:57 +0530

    Update login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,26 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
+import line from '../assets/Line.png';
+import photo from '../assets/photo.png';
+import Navbar from '../components/Navbar';
+import '../styles/LoginPage.css';
 
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-function Login() {
-    return (
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login logic
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // Reset the form
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+        <Navbar />
+    <div className='photo'><img src={photo} alt='photo' /></div>
+    <div className='logbox'>
+      <div className='line1'><img src={line} alt='line' /></div>
+      <div className='line2'><img src={line} alt='line' /></div>
+      
+      <form onSubmit={handleSubmit}>
         <div>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-                voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-                fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-                accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-                molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-                officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-                nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-                error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-                officiis modi at sunt excepturi expedita sint? Sed quibusdam
-                recusandae alias error harum maxime adipisci amet laborum.
-                Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-                cumque velit
-            </p>
+          <input className='input1'
+            type="email"
+            id="email"
+            placeholder='Email'
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
         </div>
-    )
-}
+        <div>
+          <input className='input2'
+            type="password"
+            id="password"
+            placeholder='Password'
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button className='logbtn' type="submit">LOGIN</button>
+      </form>
+      <div className='signup'>Dosen't have an an account yet?</div><div className='signup1'>Sign Up</div>
+      <div className='forgot'>Forgot Password?</div><div className='forgot1'>Click Here</div>
+    </div>
+    </div>
+  );
+};
 
-export default Login
+export default LoginPage;
