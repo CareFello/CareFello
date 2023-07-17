3a753a6cbf965c4d827fa66e5ffdb51ba482da92
praneeth <praneeth495@gmail.com>
2023-07-17 21:47:32 +0530

    add login and register functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from "./pages/Home1";
+import Home from "./pages/Home";
 import Log from "./pages/Login";
-import Home1 from "./components/Home";
+import Home1 from "./components/Test";
 import Login from "./components/Login";
 import Reg from "./components/Register";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import React from 'react'
 
-function Home() {
+function Test() {
     return (
       <div>
        <h1>Home</h1>
@@ -9,4 +9,4 @@ function Home() {
     );
   }
   
-  export default Home;
+  export default Test;
