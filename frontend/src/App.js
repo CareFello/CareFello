2a6750daaf88b61345f3866d9ea53796e0e85499
praneeth <praneeth495@gmail.com>
2023-07-17 14:48:48 +0530

    create and add login and register functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from "./pages/Home";
+import Home from "./pages/Home1";
 import Log from "./pages/Login";
+import Home1 from "./components/Home";
+import Login from "./components/Login";
+import Reg from "./components/Register";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/Login" element={<Log />} />
+          <Route path="/home" element={<Home1 />} />
+          <Route path="/log" element={<Login />} />
+          <Route path="/reg" element={<Reg />} />
 
         </Routes>
       </Router>
