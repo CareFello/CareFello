e198b5013232e389443f75cae050d84952e972da
sangeerthan99 <2020is093@stu.ucsc.cmb.ac.lk>
2023-07-21 12:25:43 +0530

    sidebar added
    sidebar added and make some changes in table, add some buttons

--- frontend/src/pages/Caregiverlist.js
+++ frontend/src/pages/Caregiverlist.js
@@ -5,90 +5,134 @@ import Table from 'react-bootstrap/Table';
 import "../styles/Caregiverlist.css"
 import Person from "./../assets/person.jpeg"
 import Navbar from "../components/Navbar";
-import Footer from '../components/HomepageFooter.js';
+import Footer from '../components/HomepageFooter';
+import Sidebar from '../components/Sidebar';
+import * as icon from "react-icons/md";
+const Caregiverlist = () => (
+  <div>
 
+    <Navbar />
+    <div className="d-flex gap-4">
+      <div className="sidbar-div">
+        <Sidebar />
+      </div>
 
-const Caregiverlist = () => {
+      <Table striped bordered hover className="0 table-height">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Reg No</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
 
+              <img className="rounded-pill" src={Person} alt="" style={{ width: "39px", height: "40px" }} />
 
-  return (
-<div>
-  <Navbar />
+            </td>
 
-    <Table striped bordered hover className="0">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Reg No</th>
-          <th>Email</th>
-          <th>Mobile</th>
-          <th>Status</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-        <td>
-        
-          <img className="rounded-pill" src={Person} alt="" style={{ width: "39px", height: "40px" }} />
-        
-        </td>
-        
-          <td>Sanath</td>
-          <td>cg-11</td>
-          <td>sanath@gmail.com</td>
-          <td>0771234567</td>
-          <td>Assigned</td>
-          <td></td>
-        </tr>
-        <tr>
-        <td>
-        
-          <img className="rounded-circle" src={Person} alt=""  style={{ width: "39px", height: "40px" }} />
-        
-        </td>
-          
-          <td>Sanath</td>
-          <td>cg-11</td>
-          <td>sanath@gmail.com</td>
-          <td>0771234567</td>
-          <td>unassigned</td>
-          <td></td>
-        </tr>
-        <tr>
-        <td>
-        
-          <img className="rounded-circle" src={Person} alt=""  style={{ width: "39px", height: "40px" }} />
-        
-        </td>
-         
-          <td>Sanath</td>
-          <td>cg-11</td>
-          <td>sanath@gmail.com</td>
-          <td>0771234567</td>
-          <td>Assigned</td>
-          <td></td>
-        </tr>
-        <tr>
-        <td>
-          <img className="rounded-circle" src={Person} alt=""  style={{ width: "39px", height: "40px" }} />
-        </td>
-          <td>Sanath</td>
-          <td>cg-11</td>
-          <td>sanath@gmail.com</td>
-          <td>0771234567</td>
-          <td>unassigned</td>
-          <td></td>
-        </tr>
-      </tbody>
-    </Table>
-<div className='contactus'>
-<Footer />
-</div>
-</div>
-);
+            <td>Sanath</td>
+            <td>cg-11</td>
+            <td>sanath@gmail.com</td>
+            <td>0771234567</td>
+            <td>Assigned</td>
+            <td>
+              <button
+                style={{
+                  backgroundColor: "transparent",
+                  color: "white",
+                  padding: "3px 3px",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                  margin: "10px",
+                }}
+                onClick={() => {
+                  console.log("Button clicked!");
+                } }
+              >
+                <icon.MdEdit size="1.5rem" color="black" /> </button>
+              <button
+                style={{
+                  backgroundColor: "transparent",
+                  color: "white",
+                  padding: "3px 5px",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                  margin: "10px",
+                }}
+                onClick={() => {
+                  console.log("Button clicked!");
+                } }
+              >
+                <icon.MdPersonPin size="1.5rem" color="blue" /> </button>
+              <button
+                style={{
+                  backgroundColor: "transparent",
+                  color: "white",
+                  padding: "3px 5px",
+                  borderRadius: "3px",
+                  cursor: "pointer",
+                  margin: "10px",
+                }}
+                onClick={() => {
+                  console.log("Button clicked!");
+                } }
+              >
+                <icon.MdDelete size="1.5rem" color="red" /> </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
 
-}
+              <img className="rounded-circle" src={Person} alt="" style={{ width: "39px", height: "40px" }} />
+
+            </td>
+
+            <td>Sanath</td>
+            <td>cg-11</td>
+            <td>sanath@gmail.com</td>
+            <td>0771234567</td>
+            <td>unassigned</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>
+
+              <img className="rounded-circle" src={Person} alt="" style={{ width: "39px", height: "40px" }} />
+
+            </td>
+
+            <td>Sanath</td>
+            <td>cg-11</td>
+            <td>sanath@gmail.com</td>
+            <td>0771234567</td>
+            <td>Assigned</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>
+              <img className="rounded-circle" src={Person} alt="" style={{ width: "39px", height: "40px" }} />
+            </td>
+            <td>Sanath</td>
+            <td>cg-11</td>
+            <td>sanath@gmail.com</td>
+            <td>0771234567</td>
+            <td>unassigned</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+    <div className='contactus'>
+      <Footer />
+    </div>
+  </div>
+)
 
 export default Caregiverlist;
