468877ba47b26c8fe6ea272e78b6ffe66c1ee1be
VinuriGamage <89873897+VinuriGamage@users.noreply.github.com>
2023-08-08 10:47:12 +0530

    Add ContactUs to home page

--- frontend/src/pages/Select.js
+++ frontend/src/pages/Select.js
@@ -1,84 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 
 
-function Select() {
+// function Select() {
   
-  const [id, setId] = useState("");
-  const [name1, setName] = useState("");
+//   const [id, setId] = useState("");
+//   const [name1, setName] = useState("");
  
 
 
-  async function add(event) {
-      event.preventDefault();
-      try {
-        await axios.post("http://localhost:8085/api/persons", {
-        name: name1,
-        id: id,
+//   async function add(event) {
+//       event.preventDefault();
+//       try {
+//         await axios.post("http://localhost:8085/api/persons", {
+//         name: name1,
+//         id: id,
         
-        });
-        alert("Person Registation Successfully");
+//         });
+//         alert("Person Registation Successfully");
 
-      } catch (err) {
-        alert(err);
-      }
-    }
-  }
+//       } catch (err) {
+//         alert(err);
+//       }
+//     }
+//   }
 
 
 
-const Select = () => {
-  const [people, setPeople] = useState([]);
+// const Select = () => {
+//   const [people, setPeople] = useState([]);
 
-  useEffect(() => {
-    // Make the GET request using Axios to fetch data from the backend
-    axios.get('http://localhost:8085/api/persons')
-      .then((response) => setPeople(response.data))
-      .catch((error) => console.error(error));
-  }, []);
+//   useEffect(() => {
+//     // Make the GET request using Axios to fetch data from the backend
+//     axios.get('http://localhost:8085/api/persons')
+//       .then((response) => setPeople(response.data))
+//       .catch((error) => console.error(error));
+//   }, []);
 
-  return (
-    <div>
-      <form>
-        <div class="form-group">
-          <label>Id</label>
-          <input type="text"  class="form-control" id="id" placeholder="id"
+//   return (
+//     <div>
+//       <form>
+//         <div class="form-group">
+//           <label>Id</label>
+//           <input type="text"  class="form-control" id="id" placeholder="id"
           
-          value={id}
-          onChange={(event) => {
-            setId(event.target.value);
-          }}
-          />
+//           value={id}
+//           onChange={(event) => {
+//             setId(event.target.value);
+//           }}
+//           />
 
-        </div>
+//         </div>
 
-        <div class="form-group">
-          <label>name</label>
-          <input type="email"  class="form-control" id="name" placeholder="name"
+//         <div class="form-group">
+//           <label>name</label>
+//           <input type="email"  class="form-control" id="name" placeholder="name"
           
-          value={name1}
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
+//           value={name1}
+//           onChange={(event) => {
+//             setName(event.target.value);
+//           }}
           
-          />
+//           />
  
-        </div>
+//         </div>
 
-        <button type="submit" class="btn btn-primary mt-4" onClick={add} >ADD</button>
-      </form>  
+//         <button type="submit" class="btn btn-primary mt-4" onClick={add} >ADD</button>
+//       </form>  
 
-      <h2>People List</h2>
-      <ul>
-        {people.map((person) => (
-          <li key={person.id}>
-            {person.id}: {person.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+//       <h2>People List</h2>
+//       <ul>
+//         {people.map((person) => (
+//           <li key={person.id}>
+//             {person.id}: {person.name}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-export default Select;
+// export default Select;
 
