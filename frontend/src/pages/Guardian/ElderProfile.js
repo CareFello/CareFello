7e684bdfdfd1ea8c3cecf79b11d77e9e29095caf
HMPS-anjalika <90828392+HMPS-anjalika@users.noreply.github.com>
2023-08-20 11:24:26 +0530

    ElderProfile UI modified

--- frontend/src/pages/Guardian/ElderProfile.js
+++ frontend/src/pages/Guardian/ElderProfile.js
@@ -1,185 +1,67 @@
-import React from 'react'
-import Sidebar from '../../components/Sidebar'
-import Box from '@mui/material/Box'
-import { Typography } from '@mui/material'
-import { CardActionArea } from '@mui/material';
-import Header from '../../components/Header'
-import Grid from '@mui/material/Grid'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Stack from '@mui/material/Stack';
-import img_1 from '../../assets/guardian/elder1.jpeg'
-import { GuardianMenuItem } from '../../components/GuardianMenuItem'
+import React from 'react';
+import Sidebar from '../../components/Sidebar';
+import Header from '../../components/Header';
+import { Box, Typography, Grid, Card, CardContent, CardMedia, IconButton } from '@mui/material';
+import { Edit } from '@mui/icons-material'; // Import the Edit icon
+import { GuardianMenuItem } from '../../components/GuardianMenuItem';
 
+import img_1 from '../../assets/guardian/elder2.jpeg';
+import img_2 from '../../assets/guardian/cb.jpg';
 import '../../styles/Guardian/ElderProfile.css';
 
+const sections = [
+  { title: 'Caregiver', image: img_2, description: 'Check out and rate/review caregiver' },
+  { title: 'Medication', image: img_2, description: 'Check out and add medication' },
+  { title: 'Daily Activities', image: img_2, description: 'Check out and add daily activities' },
+  { title: 'Daily Reports', image: img_2, description: 'Check out daily reports' },
+  { title: 'Contact Caregiver', image: img_2, description: 'Contact the caregiver' },
+  { title: 'Accommodation', image: img_2, description: 'Send accomodation requests' },
+];
 
 const ElderProfile = () => {
+
   return (
-    <div className='profile'>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center", height: "100vh", backgroundColor: "#F9FFFE"
+    }}>
       <Header />
       <Box height={80} />
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: "flex" }}>
         <Sidebar menuItems={GuardianMenuItem} />
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }} >
-          <Grid container spacing={1}>
-            <Grid item xs={9}>
-              <Stack spacing={2} direction={'row'}>
-                <Card sx={{ display: 'flex', minWidth: 30 + "%", height: 140 }}>
-                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <CardContent sx={{ flex: '1 0 auto' }}>
-                      <Typography component="div" variant="h5">
-                        78
-                      </Typography>
-                      <Typography variant="subtitle1" color="text.secondary" component="div">
-                        Active Elders
-                      </Typography>
-                    </CardContent>
-                    <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+        <Box>
 
-                    </Box>
-                  </Box>
-                  <CardMedia
-                    component="img"
-                    sx={{ Maxwidth: 75 + "%" }}
-                    image={img_1}
-                    alt="Elder Image"
-                  />
-                </Card>
+          {/* Elder's Image and Name */}
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1, p: 2 }}>
+            <CardMedia component="img" alt="Elder" height="150" width="500" src={img_1} />
+            <Typography variant="h5">Elder's Name</Typography>
+            <IconButton>
+              <Edit />
+            </IconButton>
+          </Box>
 
+          {/* Sections */}
 
-
-              </Stack>
-            </Grid>
-            <Grid container spacing={2}>
-              <Grid sx={{ flexGrow: 1, p: 3, flexDirection: 'row' }} >
-                <Card sx={{ maxWidth: 345 }}>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography variant="h5" component="div">
-                        Caregiver
-                      </Typography>
-
-                    </CardContent>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={img_1}
-                      alt="Caregiver"
-                    />
-                  </CardActionArea>
-                </Card>
-                <Card sx={{ minWidth: 175 }}>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography variant="h5" component="div">
-                        Medication
-                      </Typography>
-
-                    </CardContent>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={img_1}
-                      alt="Caregiver"
-                    />
-                  </CardActionArea>
-                </Card>
-                <Card sx={{ minWidth: 175 }}>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography variant="h5" component="div">
-                        Daily Activities
-                      </Typography>
-
-                    </CardContent>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={img_1}
-                      alt="Caregiver"
-                    />
-                  </CardActionArea>
-                </Card>
-                <Card sx={{ mixWidth: 175 }}>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography variant="h5" component="div">
-                        Daily Reports
-                      </Typography>
-
-                    </CardContent>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={img_1}
-                      alt="Caregiver"
-                    />
-                  </CardActionArea>
-                </Card>
-                <Card sx={{ mixWidth: 175 }}>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography variant="h5" component="div">
-                        Contact Caregiver
-                      </Typography>
-
-                    </CardContent>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={img_1}
-                      alt="Caregiver"
-                    />
-                  </CardActionArea>
-                </Card>
-                <Card sx={{ minWidth: 175 }}>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography variant="h5" component="div">
-                        Request Accommodation
-                      </Typography>
-
-                    </CardContent>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={img_1}
-                      alt="Caregiver"
-                    />
-                  </CardActionArea>
-                </Card>
-                <Card sx={{ minWidth: 175 }}>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography variant="h5" component="div">
-                        Complaints
-                      </Typography>
-
-                    </CardContent>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={img_1}
-                      alt="Caregiver"
-                    />
-                  </CardActionArea>
+          <Grid container spacing={2} p={2}>
+            {sections.map((section) => (
+              <Grid key={section.title} item xs={12} sm={6} md={4}>
+                <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography variant="h6" component="div">
+                      {section.title}
+                    </Typography>
+                    <Typography variant="body2">{section.description}</Typography>
+                  </CardContent>
+                  <CardMedia component="img" alt={section.title} height="100" src={section.image} />
                 </Card>
               </Grid>
-            </Grid>
+            ))}
           </Grid>
         </Box>
-
       </Box>
+    </div>
+  );
+};
 
-
-
-    </div >
-  )
-}
-
-export default ElderProfile
-
-
-
+export default ElderProfile;
