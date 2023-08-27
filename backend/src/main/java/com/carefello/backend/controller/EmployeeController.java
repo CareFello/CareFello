ea664c1ebcbc7bfe86264885d4d74631d8dc5090
praneeth <praneethjayarathna7@gmail.com>
2023-08-27 13:45:26 +0530

    forgot password

--- backend/src/main/java/com/carefello/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/carefello/backend/controller/EmployeeController.java
@@ -2,8 +2,12 @@ package com.carefello.backend.controller;
 
 import com.carefello.backend.DTO.EmployeeDTO;
 import com.carefello.backend.DTO.LoginDTO;
+import com.carefello.backend.model.Employee;
 import com.carefello.backend.service.EmployeeService;
 import com.carefello.backend.payload.response.LoginMesage;
+import com.carefello.backend.repo.EmployeeRepo;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +18,10 @@ import org.springframework.web.bind.annotation.*;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private EmployeeRepo employeeRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @PostMapping(path = "/save")
     public String saveEmployee(@RequestBody EmployeeDTO employeeDTO)
     {
@@ -25,5 +33,14 @@ public class EmployeeController {
     {
         LoginMesage loginResponse = employeeService.loginEmployee(loginDTO);
         return ResponseEntity.ok(loginResponse);
+    }
+
+    @PutMapping("/updatepass/{resetemail}")
+    public ResponseEntity<?> updatepass1(@PathVariable String resetemail, @RequestBody EmployeeDTO updatpass){
+        Employee employee = employeeRepo.findByEmail(resetemail);
+        employee.setPassword(this.passwordEncoder.encode(updatpass.getPassword()));
+        employee = employeeRepo.save(employee);
+
+        return ResponseEntity.ok(employee);
     }
 }
--- backend/src/main/java/com/carefello/backend/service/EmailService.java
+++ backend/src/main/java/com/carefello/backend/service/EmailService.java
@@ -4,12 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import com.carefello.backend.payload.response.Forgotemail;
+import com.carefello.backend.repo.EmployeeRepo;
+import com.carefello.backend.DTO.EmailDTO;
+import com.carefello.backend.model.Employee;
 
 @Service
 public class EmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+    @Autowired
+    private EmployeeRepo employeeRepo1;
 
     
     // public EmailService(JavaMailSender javaMailSender) {
@@ -24,5 +31,25 @@ public class EmailService {
         message.setText(text);
 
         javaMailSender.send(message);
+    }
+
+    public Forgotemail checkEmail(String email1){
+        Employee employee = employeeRepo1.findByEmail(email1);
+        if (employee != null){
+            int random = employee.getCode();
+            // sendEmail(email1, "Verification code", "Your verification code is "+random);
+            return new Forgotemail("true", random);
+        }else{
+            return new Forgotemail("false", 0000);
+        }
+    }
+
+    public Forgotemail checkCode(int code){
+        Employee employee = employeeRepo1.findByCode(code);
+        if (employee != null){
+            return new Forgotemail("true", 0000);
+        }else{
+            return new Forgotemail("false", 0000);
+        }
     }
 }
