d433fdd7ec238c34c822fd1de52b86d018425e21
praneeth <praneethjayarathna7@gmail.com>
2023-09-08 19:53:24 +0530

    doctot timeslot updation

---
+++ backend/src/main/java/com/carefello/backend/DTO/DoctortimeDTO.java
@@ -0,0 +1,26 @@
+package com.carefello.backend.DTO;
+
+import lombok.Data;
+
+import java.sql.Time;
+
+
+
+@Data
+public class DoctortimeDTO {
+    
+    private Time sunday;
+    private Time sunday1;
+    private Time monday;
+    private Time monday1;
+    private Time tuesday;
+    private Time tuesday1;
+    private Time wednesday;
+    private Time wednesday1;
+    private Time thusrday;
+    private Time thusrday1;
+    private Time friday;
+    private Time friday1;
+    private Time saturday;
+    private Time saturday1;
+}
