fc8a46793211174c6775eedcd589718c1cf6a843
AchalaHM <2020cs140@stu.ucsc.cmb.ac.lk>
2023-09-15 14:44:16 +0530

    fix issues in complaints

--- backend/src/main/java/com/carefello/backend/model/Complaint.java
+++ backend/src/main/java/com/carefello/backend/model/Complaint.java
@@ -15,4 +15,46 @@ public class Complaint {
     @ManyToOne
     @JoinColumn(name = "guardian_id")
     private Guardian guardian;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Guardian getGuardian() {
+        return guardian;
+    }
+
+    public void setGuardian(Guardian guardian) {
+        this.guardian = guardian;
+    }
+
+    public Complaint() {
+    }
+
+    public Complaint(int id, String type, String details, Guardian guardian) {
+        this.id = id;
+        this.type = type;
+        this.details = details;
+        this.guardian = guardian;
+    }
 }
