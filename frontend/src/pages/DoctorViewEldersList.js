219588a55e509f67a7c17423f50d6156dfabc9d2
sangeerthan99 <2020is093@stu.ucsc.cmb.ac.lk>
2023-09-19 13:23:52 +0530

    fix bugs

--- frontend/src/pages/DoctorViewEldersList.js
+++ frontend/src/pages/DoctorViewEldersList.js
@@ -10,6 +10,7 @@ import { DoctorMenuItem } from '../components/DoctorMenuItem';
 import Box from '@mui/material/Box';
 import Img1 from '../assets/E4.jpg';
 import Img2 from '../assets/E5.jpg';
+import Img3 from '../assets/E3.jpg';
 const dataFromDatabase = [
   {
     id: 1,
@@ -25,6 +26,13 @@ const dataFromDatabase = [
     Guardian: 'Shehan Gunasekara', 
     imageUrl: Img2,
   },
+    {
+    id: 3,
+    Name: 'M. Harishan',
+    Age: '65',
+    Guardian: 'Kishan Harishan',
+    imageUrl: Img3,
+    },
   // Add more data items as needed
 ];
 
@@ -49,15 +57,19 @@ const DoctorViewEldersList = () => {
           {dataFromDatabase.map((elder) => (
             <Grid item xs={2.5} sm={2.5} md={2.5} key={elder.id}>
               <Link to={`/elder-details/${elder.id}`} style={{ textDecoration: 'none' }}>
+                
                 <Card>
                 <img src={elder.imageUrl} alt={elder.title} style={cardImageStyle} />
                   <CardContent>
+                   
                     <Typography item xs={4} sm={4} md={4} variant="h6" component="div">
                       {elder.Name}
                     </Typography>
+                    
                     <Typography variant="body2" color="text.secondary">
                      Age: {elder.Age}
                     </Typography>
+                    
                     <Typography variant="body2" color="primary.main">
                      Guardian: {elder.Guardian}
                     </Typography>
