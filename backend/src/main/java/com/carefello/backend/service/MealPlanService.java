1ffb2abe2958fb20e0960f4d9bb5aed922ed625a
AchalaHM <2020cs140@stu.ucsc.cmb.ac.lk>
2023-09-22 17:15:19 +0530

    Update Meal plan codes

--- backend/src/main/java/com/carefello/backend/service/MealPlanService.java
+++ backend/src/main/java/com/carefello/backend/service/MealPlanService.java
@@ -1,10 +1,11 @@
 package com.carefello.backend.service;
 
 import com.carefello.backend.DTO.MealPlanDTO;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface MealPlanService {
-    void addMealPlan(MealPlanDTO mealPlanDTO);
+    void addMealPlan(MealPlanDTO mealPlanDTO, MultipartFile imageFile);
     List<MealPlanDTO> getAllMealPlans();
 }
