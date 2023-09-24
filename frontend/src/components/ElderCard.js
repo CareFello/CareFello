8785aa94ec04069c99a750af0116c94bfd4317ab
AchalaHM <2020cs140@stu.ucsc.cmb.ac.lk>
2023-09-24 16:29:37 +0530

    Update Elder Card and fix issues in add elder

--- frontend/src/components/ElderCard.js
+++ frontend/src/components/ElderCard.js
@@ -2,15 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Card, CardContent, Typography, CardMedia, Grid } from '@mui/material';
 import { NavLink } from 'react-router-dom';
-import pro from "../assets/avatar.png"
+import pro from "../assets/avatar.png";
 
 function ElderCard({ guardianId }) {
   const [elderList, setElderList] = useState([]);
 
-  // const url = "http://localhost:8080/api/v1/guardian/${guardianId}/elders/viewElderByGuardianId";
-
   useEffect(() => {
-    // Make an HTTP GET request to fetch elders' details and images
+    // Make an HTTP GET request to fetch elders' details including images
     axios.get(`http://localhost:8080/api/v1/guardian/${guardianId}/elders/viewElderByGuardianId`)
       .then((response) => {
         console.log(response.data);
@@ -26,7 +24,6 @@ function ElderCard({ guardianId }) {
       <Grid container spacing={2}>
         {elderList.map((elder) => (
           <Grid item key={elder.id} xs={12} sm={6} md={3} lg={3}>
-
             <NavLink to={`/elder/${elder.id}?guardianId=${guardianId}`} style={{ textDecoration: 'none' }}>
               <Card
                 style={{
@@ -40,13 +37,19 @@ function ElderCard({ guardianId }) {
                   component="img"
                   height="100"
                   alt={elder.name}
-                  image={elder.imagePath ? `data:image/jpeg;base64,${elder.imagePath}` : pro}
-                // Use the image path directly from the elder object
+                  // Use the image data directly from the elder object if it's not null, otherwise use the pro image
+                  src={elder.image ? `data:image/jpeg;base64,${elder.image}` : pro}
+                  onError={(e) => {
+                    // If there's an error loading the image, use a default image (pro image)
+                    e.target.src = pro;
+                  }}
                 />
+
                 <CardContent style={{ flex: 1 }}>
                   <Typography variant="h6">{elder.name}</Typography>
                   <Typography variant="body2">{elder.relationship}</Typography>
-                  <Typography variant="body2">{elder.image}</Typography>
+                  {/* You may want to remove this line as it displays the image data */}
+                  {/* <Typography variant="body2">{elder.image}</Typography> */}
                 </CardContent>
               </Card>
             </NavLink>
@@ -58,8 +61,3 @@ function ElderCard({ guardianId }) {
 }
 
 export default ElderCard;
-
-
-
-
-
