ac9fab816ce8416220429c6ef57874ee53a7ea9a
VinuriGamage <89873897+VinuriGamage@users.noreply.github.com>
2023-09-26 12:10:50 +0530

    Create new login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,11 +11,11 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment';
-
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
 import { Button } from '@mui/material';
 import TextField from '@mui/material/TextField';
+import login_img from '../assets/login_img.png';
 
 Model.setAppElement("#root");
 
@@ -157,42 +157,55 @@ function Login() {
   }
 
   return (
-    <div>
-      <Navbar />
-      <div className='photo'><img src={photo} alt='photo' /></div>
-      <div className='logbox'>
-        <div className='line1'><img src={line} alt='line' /></div>
-        <div className='line2'><img src={line} alt='line' /></div>
-
-        <form>
-          <div>
-            <input className='input1'
-              type="email"
-              id="email"
-              placeholder='Email'
-              value={email}
-              onChange={(event) => {
+    <div> 
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      {/* <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"> */}
+          {/* <img class="w-8 h-8 mr-2" src="login_img" alt="login_img"></img> */}
+            
+      {/* </a> */}
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign In
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input type="email" name="email" id="email"  value={email} onChange={(event) => {
                 setEmail(event.target.value);
               }}
-              required
-            />
-          </div>
-          <div>
-            <input className='input2'
-              type="password"
-              id="password"
-              placeholder='Password'
-              value={password}
-              onChange={(event) => {
+              required class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""></input>
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" value={password} placeholder="••••••••" onChange={(event) => {
                 setPassword(event.target.value);
               }}
               required
-            />
+             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""></input>
+                  </div>
+                  <div class="flex items-center justify-between">
+                      <div class="flex items-start">
+                          <div class="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 mt-9 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""></input>
+                            <div class="ml-3 text-sm">
+                            <label for="remember" class="text-gray-500  dark:text-gray-300">Remember me</label>
+                          </div>
+                          </div>
+                          
+                      </div>
+                      <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500"><Button onClick={openFirstModal}>Forgot password?</Button></a>
+                  </div>
+                  <button type="submit" onClick={login} class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <a href='http://localhost:3000/Registration' class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  </p>
+              </form>
           </div>
-          <button className='logbtn' type="submit" onClick={login}>LOGIN</button>
-        </form>
-        <div className='signup'>Dosen't have an an account yet?</div><div className='signup1'><a href='http://localhost:3000/Registration'>Sign Up</a></div>
-        <div className='forgot'>Forgot Password?</div><div className='forgot1'><Button onClick={openFirstModal}>Click Here</Button></div>
+      </div>
+  </div>
+
+        
 
         <Model isOpen={firstModalIsOpen}
           onRequestClose={closeFirstModal} style={{
@@ -306,7 +319,7 @@ function Login() {
         </Model>
 
       </div>
-    </div >
+    
   );
 };
 
