8393f08255ee8dc7dfad0bb5b85eb47ea64d7efc
ramza <ramzamohideen@gmail.com>
2023-10-30 16:49:22 +0530

    change the name of function and few other

--- frontend/src/pages/Guardian/GuardianViewHealthTips.js
+++ frontend/src/pages/Guardian/GuardianViewHealthTips.js
@@ -18,11 +18,11 @@ import AddIcon from "@mui/icons-material/Add";
 import Modal from "@mui/material/Modal";
 import { TextareaAutosize as BaseTextareaAutosize } from "@mui/base/TextareaAutosize";
 import { styled } from "@mui/system";
-import { GuardianMenuItem } from "../../components/GuardianMenuItem";
+import { GuardianMenuItem } from "../../components/GuardianMenuItem"; 
 
 //import "../../styles/DoctorViewHealthTips.css";
 
-const DoctorViewHealthTips = () => {
+const GuardianViewHealthTips = () => {
   const user = {
     name: "Dr. John Doe",
     profileImage: "https://via.placeholder.com/50", // Example placeholder image URL
@@ -57,99 +57,21 @@ const DoctorViewHealthTips = () => {
     setSearchTerm(e.target.value);
   };
 
-  const handleAddPost = () => {
-    if (newPost.title && newPost.content) {
-      setPosts([...posts, { id: posts.length + 1, ...newPost }]);
-      setNewPost({ title: "", content: "" });
-      setOpenModal(false); // Close modal after adding the post
-    }
-  };
 
-  const handleDeletePost = (id) => {
-    const updatedPosts = posts.filter((post) => post.id !== id);
-    setPosts(updatedPosts);
-  };
-  const blue = {
-    100: "#DAECFF",
-    200: "#b6daff",
-    400: "#3399FF",
-    500: "#007FFF",
-    600: "#0072E5",
-    900: "#003A75",
-  };
 
-  const grey = {
-    50: "#F3F6F9",
-    100: "#E5EAF2",
-    200: "#DAE2ED",
-    300: "#C7D0DD",
-    400: "#B0B8C4",
-    500: "#9DA8B7",
-    600: "#6B7A90",
-    700: "#434D5B",
-    800: "#303740",
-    900: "#1C2025",
-  };
-
-  const Textarea = styled(BaseTextareaAutosize)(
-    ({ theme }) => `
-    width: 500px;
-    font-family: IBM Plex Sans, sans-serif;
-    font-size: 0.875rem;
-    font-weight: 400;
-    line-height: 1.5;
-    padding: 8px 12px;
-    margin: 4px;
-    border-radius: 8px;
-    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
-    background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
-   border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
-    box-shadow: 0px 2px 2px ${
-      theme.palette.mode === "dark" ? grey[900] : grey[50]
-    };
-
-    &:hover {
-      border-color: ${blue[400]};
-    }
-
+ 
   
-
-    // firefox
-    &:focus-visible {
-      outline: 0;
-    }
-  `
-  );
   return (
     <div className="HealthTip">
       <Header />
       <Box height={80} />
       {/* <Box sx={{ display: "flex" }}> */}
       <Sidebar menuItems={GuardianMenuItem} />
-      {/* <Box component="main" sx={{ flexGrow: 1, p: 3 }}></Box> */}
+
       <div>
         <Box display="flex" justifyContent="center">
           <Box width="60%" textAlign="left">
-            {/* <Stack sx={6}>
-                <Typography variant="h4" gutterBottom textAlign="start">
-                  Health Tips
-                </Typography>
-              </Stack>
-       */}
-
             <Box width="100%" maxWidth="600px" mx="auto">
-              {/* <Paper
-                component="form"
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  width: "100%",
-                  padding: 2,
-                  marginBottom: 2,
-                  height: "fit-content",
-                  borderRadius: "4px",
-                }}
-              > */}
               <Grid container>
                 <Grid xs={12} sx={{ marginBottom: "45px" }}>
                   <Typography variant="h4" gutterBottom textAlign="start">
@@ -184,7 +106,7 @@ const DoctorViewHealthTips = () => {
                   </IconButton>
                 </Grid>
               </Grid>
-              {/* </Paper> */}
+
               <List>
                 {posts
                   .filter((post) =>
@@ -214,13 +136,11 @@ const DoctorViewHealthTips = () => {
 
                       <Grid container>
                         <Grid xs={10} sx={{ padding: "5px 5px 5px 16px" }}>
-                          {/* <Box p={2} sx={{ borderRadius: "4px", backgroundColor: "#f9f9f9" }}> */}
                           <ListItemText
                             primary={post.title}
                             secondary={post.content}
                             sx={{ borderRadius: "4px" }}
                           />
-                         
                         </Grid>
                       </Grid>
                     </Box>
@@ -233,4 +153,4 @@ const DoctorViewHealthTips = () => {
     </div>
   );
 };
-export default DoctorViewHealthTips;
+export default GuardianViewHealthTips;
