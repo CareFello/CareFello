78ac5ed8de466d789e977e113ffccb88713228c9
ramza <ramzamohideen@gmail.com>
2023-10-30 16:26:45 +0530

    doctor health tips view and add modal

--- frontend/src/pages/Doctor/DoctorViewHealthTips.js
+++ frontend/src/pages/Doctor/DoctorViewHealthTips.js
@@ -16,8 +16,8 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import Modal from "@mui/material/Modal";
-import { TextareaAutosize as BaseTextareaAutosize } from "@mui/base/TextareaAutosize";
-import { styled } from "@mui/system";
+// import { TextareaAutosize as BaseTextareaAutosize } from "@mui/base/TextareaAutosize";
+// import { styled } from "@mui/system";
 import { GuardianMenuItem } from "../../components/GuardianMenuItem";
 
 //import "../../styles/DoctorViewHealthTips.css";
@@ -69,57 +69,55 @@ const DoctorViewHealthTips = () => {
     const updatedPosts = posts.filter((post) => post.id !== id);
     setPosts(updatedPosts);
   };
-  const blue = {
-    100: "#DAECFF",
-    200: "#b6daff",
-    400: "#3399FF",
-    500: "#007FFF",
-    600: "#0072E5",
-    900: "#003A75",
-  };
+  // const blue = {
+  //   100: "#DAECFF",
+  //   200: "#b6daff",
+  //   400: "#3399FF",
+  //   500: "#007FFF",
+  //   600: "#0072E5",
+  //   900: "#003A75",
+  // };
 
-  const grey = {
-    50: "#F3F6F9",
-    100: "#E5EAF2",
-    200: "#DAE2ED",
-    300: "#C7D0DD",
-    400: "#B0B8C4",
-    500: "#9DA8B7",
-    600: "#6B7A90",
-    700: "#434D5B",
-    800: "#303740",
-    900: "#1C2025",
-  };
+  // const grey = {
+  //   50: "#F3F6F9",
+  //   100: "#E5EAF2",
+  //   200: "#DAE2ED",
+  //   300: "#C7D0DD",
+  //   400: "#B0B8C4",
+  //   500: "#9DA8B7",
+  //   600: "#6B7A90",
+  //   700: "#434D5B",
+  //   800: "#303740",
+  //   900: "#1C2025",
+  // };
 
-  const Textarea = styled(BaseTextareaAutosize)(
-    ({ theme }) => `
-    width: 500px;
-    font-family: IBM Plex Sans, sans-serif;
-    font-size: 0.875rem;
-    font-weight: 400;
-    line-height: 1.5;
-    padding: 8px 12px;
-    margin: 4px;
-    border-radius: 8px;
-    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
-    background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
-   border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
-    box-shadow: 0px 2px 2px ${
-      theme.palette.mode === "dark" ? grey[900] : grey[50]
-    };
+  // const Textarea = styled(BaseTextareaAutosize)(
+  //   ({ theme }) => `
+  //   width: 500px;
+  //   font-family: IBM Plex Sans, sans-serif;
+  //   font-size: 0.875rem;
+  //   font-weight: 400;
+  //   line-height: 1.5;
+  //   padding: 8px 12px;
+  //   margin: 4px;
+  //   border-radius: 8px;
+  //   color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  //   background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
 
-    &:hover {
-      border-color: ${blue[400]};
-    }
+  //   box-shadow: 0px 2px 2px ${
+  //     theme.palette.mode === "dark" ? grey[900] : grey[50]
+  //   };
 
-  
+  //   &:hover {
+  //     border-color: ${blue[400]};
+  //   }
 
-    // firefox
-    &:focus-visible {
-      outline: 0;
-    }
-  `
-  );
+  //   // firefox
+  //   &:focus-visible {
+  //     outline: 0;
+  //   }
+  // `
+  // );
   return (
     <div className="HealthTip">
       <Header />
@@ -130,29 +128,10 @@ const DoctorViewHealthTips = () => {
       <div>
         <Box display="flex" justifyContent="center">
           <Box width="60%" textAlign="left">
-            {/* <Stack sx={6}>
-                <Typography variant="h4" gutterBottom textAlign="start">
-                  Health Tips
-                </Typography>
-              </Stack>
-       */}
-
             <Box width="100%" maxWidth="600px" mx="auto">
-              {/* <Paper
-                component="form"
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  width: "100%",
-                  padding: 2,
-                  marginBottom: 2,
-                  height: "fit-content",
-                  borderRadius: "4px",
-                }}
-              > */}
               <Grid container>
                 <Grid xs={12} sx={{ marginBottom: "45px" }}>
-                  <Typography variant="h4" gutterBottom textAlign="start">
+                  <Typography variant="h3" gutterBottom textAlign="start">
                     Health Tips
                   </Typography>
                 </Grid>
@@ -184,7 +163,7 @@ const DoctorViewHealthTips = () => {
                   </IconButton>
                 </Grid>
               </Grid>
-              {/* </Paper> */}
+
               <List>
                 {posts
                   .filter((post) =>
@@ -224,8 +203,7 @@ const DoctorViewHealthTips = () => {
                         </Grid>
                         <Grid
                           xs={2}
-                          justifyContent="center" // Center content horizontally
-                          alignItems="center"
+                          justifyContent="center"
                           sx={{ padding: 4 }}
                         >
                           <IconButton
@@ -244,14 +222,6 @@ const DoctorViewHealthTips = () => {
             <Box
               sx={{ position: "fixed", bottom: 80, right: 200, zIndex: 1000 }}
             >
-              {/* <Button
-                variant="contained"
-                color="primary"
-                sx={{ borderRadius: 2 }}
-                onClick={handleAddPost}
-              >
-                A */}
-              {/* </Button> */}
               <Fab
                 color="primary"
                 aria-label="add"
@@ -281,9 +251,15 @@ const DoctorViewHealthTips = () => {
                   Add Health Tips
                 </Typography>
                 <Box
+                  sx={{
+                    width: 500,
+                    maxWidth: "100%",
+                  }}
+                ></Box>
+                <Box
                   display="flex"
                   alignItems="center"
-                  sx={{ marginBottom: "20px" }}
+                  sx={{ marginBottom: "20px", mt: 4 }}
                 >
                   <Avatar
                     alt={user.name}
@@ -292,80 +268,98 @@ const DoctorViewHealthTips = () => {
                   />
                   <Typography>{user.name}</Typography>
                 </Box>
-                <Box
-                  display="flex"
+                {/* <Box
+                  // display="flex"
                   alignItems="center"
-                  sx={{ "& .MuiTextField-root": { m: 1, width: "50ch" } }}
-                >
-                  <Grid container>
-                    <Grid xs={12} sx={{ margin: "20px" }}>
-                      <TextField
-                        label="Topic"
-                        value={newPost.title}
-                        onChange={(e) =>
-                          setNewPost({ ...newPost, title: e.target.value })
-                        }
-                        fullWidth
-                        margin="normal"
-                        sx={{
-                          width: "100%",
-                          marginBottom: "20px",
-                          "& .MuiInputBase-input": {
-                            borderRadius: "14px",
-                          },
-                          "& .MuiInputBase-root": {
-                            borderRadius: "14px",
-                          },
-                          
-                        }}
-                      />
-                    </Grid>
-                    <Grid xs={12}>
-                      <TextField
-                        label="Health Tip Description"
-                        value={newPost.content}
-                        onChange={(e) =>
-                          setNewPost({ ...newPost, content: e.target.value })
-                        }
-                        multiline
-                        rows={12}
-                        fullWidth
-                        margin="normal"
-                        sx={{
-                          width: "100%",
-                          marginBottom: "20px",
-                          "& .MuiInputBase-input": {
-                            borderRadius: "14px",
-                          },
-                          "& .MuiInputBase-root": {
-                            borderRadius: "14px",
-                          },
-                        }}
-                      />
-
-                      {/* <Textarea
-                        aria-label="minimum height"
-                        minRows={15}
-                        placeholder="Enter the health tip"
-                        value={newPost.content}
-                        onChange={(e) =>
-                          setNewPost({ ...newPost, content: e.target.value })
-                        }
-                        fullWidth
-                      /> */}
-                    </Grid>
-                    <Grid xs={12}>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={handleAddPost}
-                        sx={{ mt: 2 }} // Add margin top to space out the button
-                      >
-                        Post
-                      </Button>
-                    </Grid>
+                  
+                  sx={{ "& .MuiTextField-root": { m: 1, width: "50ch",backgroundColor:"red" } }}
+                > */}
+                <Grid container>
+                  {/* <Grid container spacing={2}> */}
+                  <Grid item xs={12} sx={{ marginTop: 3, marginBottom: 3 }}>
+                    {/* <TextField
+                          label="Topic"
+                          value={newPost.title}
+                          onChange={(e) =>
+                            setNewPost({ ...newPost, title: e.target.value })
+                          }
+                          fullWidth
+                        /> */}
+                    <Typography variant="h8">Topic</Typography>
+                    <TextField
+                      fullWidth
+                      id="fullWidth"
+                      value={newPost.title}
+                      onChange={(e) =>
+                        setNewPost({ ...newPost, title: e.target.value })
+                      }
+                      sx={{
+                        "& .MuiOutlinedInput-multiline": {
+                          border: "none", // Remove the border from the multiline input
+                        },
+                      }}
+                    />
                   </Grid>
-                </Box>
+                  <Grid item xs={12} sx={{ marginTop: 3, marginBottom: 3 }}>
+                    {/* <TextField
+                          label="Health Tip Description"
+                          value={newPost.content}
+                          onChange={(e) =>
+                            setNewPost({ ...newPost, content: e.target.value })
+                          }
+                          multiline
+                          rows={10}
+                          fullWidth
+                        /> */}
+                    <Typography variant="h8">Description</Typography>
+                    <TextField
+                      fullWidth
+                      id="fullWidth"
+                      value={newPost.content}
+                      onChange={(e) =>
+                        setNewPost({ ...newPost, content: e.target.value })
+                      }
+                      multiline
+                      rows={6}
+                      sx={{
+                        "& .MuiOutlinedInput-multiline": {
+                          border: "none", // Remove the border from the multiline input
+                        },
+                        "& .MuiOutlinedInput-outlined": {
+                          border: "none", // Remove the border from the multiline input
+                        },
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                          borderWidth: "1px", // Set the border width to 1px
+                        },
+                        "& .MuiOutlinedInput-root": {
+                          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                            border: "none", // Remove the border when the input is focused
+                          },
+                          "&:hover .MuiOutlinedInput-notchedOutline": {
+                            border: "none", // Remove the border when hovered
+                          },
+                          border: "none", // Remove the default border
+                        },
+                      }}
+                    
+
+                    />
+                  </Grid>
+
+                  {/* </G//rid> */}
+                  <Grid xs={12}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleAddPost}
+                      sx={{ mt: 5 }} // Add margin top to space out the button
+                      fullWidth
+                    >
+                      Post
+                    </Button>
+                  </Grid>
+                </Grid>
+                {/* </Box> */}
               </Box>
             </Modal>
             {/* <Box width="60%" textAlign="center">
