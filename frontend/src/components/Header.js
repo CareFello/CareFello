e27e9f84a4b81d39c0bc050391c3013330943281
VinuriGamage <89873897+VinuriGamage@users.noreply.github.com>
2023-11-01 08:36:52 +0530

    Fix styles in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -218,7 +218,7 @@ export default function Header() {
                         <Box sx={{ flexGrow: 1 }} />
                         <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
                             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-                                <Badge badgeContent={4} color="error">
+                                <Badge color="error">
                                     <MailIcon />
                                 </Badge>
                             </IconButton>
@@ -227,7 +227,7 @@ export default function Header() {
                                 aria-label="show 17 new notifications"
                                 color="inherit"
                             >
-                                <Badge badgeContent={17} color="error">
+                                <Badge color="error">
                                     <NotificationsIcon />
                                 </Badge>
                             </IconButton>
